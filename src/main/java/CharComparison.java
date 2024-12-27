51d933bc7d2e507195d4a2b807d0cbedc34c6586
chnwachu20 <chnwachu@gmail.com>
2024-12-27 17:26:30 +0000

    Completed?

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -1,4 +1,6 @@
-public class CharComparison {
+public class CharComparison 
+
+{
     /**
      * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
      * That means its position in a dictionary, eg:
@@ -11,6 +13,26 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        int a_len = a.length;
+    int b_len = b.length;
+    
+    if (a_len > b_len){
+        return 1;}
+    if (b_len > a_len){
+        return -1;}
+        for (int i = 0; i < a.length; i++) {
+        if (a[i] != b[i]){
+            String stra = String.valueOf(a[i]); 
+            String strb = String.valueOf(b[i]);
+            int c=stra.compareTo(strb);
+            if (c> 0){
+                return 1;}else{
+                    return -1;
+                }
+        }
+       
+    }
+    return 0;
     }
 }
+
